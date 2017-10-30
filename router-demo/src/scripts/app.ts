52c3b33705a07f2f301e587ac30478c6fe6e7c67
Brandon <brandonyoyoslade@gmail.com>
2017-10-30 17:14:26 -0600

    fix(router-demo): fix absolute paths; ensure http-server is using push-state

--- router-demo/src/scripts/app.ts
+++ router-demo/src/scripts/app.ts
@@ -2,6 +2,6 @@ import { Router } from '@aboveyou00/capstone-router';
 
 (async () => {
     let router = new Router();
-    await router.dependencyLoader.loadSchema('dependencies.json');
+    await router.dependencyLoader.loadSchema('/dependencies.json');
     await router.loadRoutes('routes');
 })();
