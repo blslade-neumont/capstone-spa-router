ccae3e366ed97bb653a314eab709ff34c5181ba9
Brandon <brandonyoyoslade@gmail.com>
2017-11-01 18:21:56 -0600

    feat(launchpad): add launchpad project to build and launch all other projects concurrently

---
+++ launchpad/config/webpack/development.js
@@ -0,0 +1,55 @@
+let { ContextReplacementPlugin, DefinePlugin } = require('webpack');
+let path = require('path');
+let webpackMerge = require('webpack-merge');
+let commonConfig = require('./common');
+let clientConfig = require('./client-config');
+
+const ROOT = path.resolve(__dirname, '../../');
+
+//Loaders
+let $awesomeTypescript = {
+    loader: 'awesome-typescript-loader'
+};
+let $style = {
+    loader: 'style-loader'
+};
+let $trim = {
+    loader: 'trim-loader'
+};
+let $sass = {
+    loader: 'sass-loader',
+    options: {
+        includePaths: [
+            path.resolve(ROOT, 'src/styles')
+        ]
+    }
+};
+
+let devConfig = {
+    entry: {
+        'styles': './src/styles/styles.scss',
+        'app': './src/scripts/app.ts'
+    },
+
+    output: {
+        path: path.resolve(ROOT, 'dist/scripts')
+    },
+
+    plugins: [
+        new DefinePlugin(clientConfig)
+    ],
+
+    module: {
+        loaders: [
+            { test: /\.ts$/, loaders: [$awesomeTypescript], exclude: /\.spec\.ts$/ },
+            { test: /\.scss$/, loaders: [$style, $trim, $sass] }
+        ]
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        watchOptions: { aggregateTimeout: 300, poll: 1000 }
+    }
+};
+
+module.exports = webpackMerge(commonConfig, devConfig);
