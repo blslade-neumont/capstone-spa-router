472224838a1cdb276fd0bfa2b991be2675fd1c66
Brandon <brandonyoyoslade@gmail.com>
2017-11-09 00:47:17 -0700

    feat(launchpad): add problem definition animations

--- launchpad/src/scripts/app.ts
+++ launchpad/src/scripts/app.ts
@@ -2,6 +2,8 @@ import { Router } from '@aboveyou00/capstone-router';
 
 (async () => {
     let router = new Router();
-    await router.dependencyLoader.loadSchema('/dependencies.json');
+    (<any>window).router = router;
+    (<any>window).navigateTo = router.navigateTo.bind(router);
+    await router.dependencyLoader.loadSchema('/router-dependencies.json');
     await router.loadRoutes('routes');
 })();
