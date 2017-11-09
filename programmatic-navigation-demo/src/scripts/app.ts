5a49bdd18aa7430551d77e7735fd167f246204f3
Brandon <brandonyoyoslade@gmail.com>
2017-11-08 21:37:51 -0700

    feat(programmatic-navigation-demo): move table of contents to the left side of the screen

--- programmatic-navigation-demo/src/scripts/app.ts
+++ programmatic-navigation-demo/src/scripts/app.ts
@@ -1,4 +1,5 @@
 import { Router } from '@aboveyou00/capstone-router';
+import 'rxjs/add/operator/filter';
 
 let router: Router;
 let isLoaded = false;
@@ -6,43 +7,32 @@ let isLoaded = false;
     router = new Router();
     await router.dependencyLoader.loadSchema('/dependencies.json');
     await router.loadRoutes('routes');
+    router.events.filter(e => e.type == 'begin').subscribe(navEvent => {
+        let navHistory = document.getElementById('navigationHistory');
+        let anchor = document.createElement('a');
+        anchor.href = '#';
+        anchor.innerHTML = navEvent.path;
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            e.stopPropagation();
+            router.navigateTo(navEvent.path);
+        });
+        if (navHistory.firstChild) navHistory.insertBefore(anchor, navHistory.firstChild);
+        else navHistory.appendChild(anchor);
+    });
     isLoaded = true;
 })();
 
-let pathInput = <HTMLInputElement>document.getElementById('pathInput');
-let beginNavigationButton = <HTMLButtonElement>document.getElementById('beginNavigationButton');
-pathInput.addEventListener('keyup', e => {
-    if (e.key !== 'Enter') return;
-    e.preventDefault();
-    e.stopPropagation();
-    beginNavigationButton.click();
-});
-function beginNavigation(e) {
+(<any>window).beginNavigation = function(e) {
     if (!isLoaded) {
         alert(`Please wait for the dependency and route schema to load.`);
         return false;
     }
+    let pathInput = <HTMLInputElement>document.getElementById('pathInput');
     let val = pathInput.value;
     console.log(val);
     pathInput.value = '';
     router.navigateTo(val);
     
-    let navHistory = document.getElementById('navigationHistory');
-    let anchor = document.createElement('a');
-    anchor.href = '#';
-    anchor.innerHTML = val;
-    anchor.addEventListener('click', e => {
-        e.preventDefault();
-        e.stopPropagation();
-        router.navigateTo(val);
-    });
-    if (navHistory.firstChild) navHistory.insertBefore(anchor, navHistory.firstChild);
-    else navHistory.appendChild(anchor);
-    
     return false;
 }
-beginNavigationButton.addEventListener('click', e => {
-    e.preventDefault();
-    e.stopPropagation();
-    return beginNavigation(e);
-});
