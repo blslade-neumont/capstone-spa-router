51da2d54e56682db0f5186891fc7b64e6d96daf7
Brandon <brandonyoyoslade@gmail.com>
2017-11-19 16:19:57 -0700

    test(capstone-router): add unit tests for util/createPreloadStrategy

--- capstone-router/src/router/preload-strategy/create-preload-strategy.ts
+++ capstone-router/src/router/preload-strategy/create-preload-strategy.ts
@@ -4,7 +4,7 @@ import { FollowLinksPreloadStrategy } from './follow-links-preload-strategy';
 import { PreloadEverythingStrategy } from './preload-everything-strategy';
 
 export function createPreloadStrategy(strategy?: 'none' | 'follow-links' | 'all' | PreloadStrategy): PreloadStrategy {
-    if (!strategy) strategy = 'none';
+    if (typeof strategy === 'undefined') strategy = 'none';
     
     if (strategy instanceof PreloadStrategy) return strategy;
     
--- capstone-router/src/router/preload-strategy/test/create-preload-strategy.spec.ts
+++ capstone-router/src/router/preload-strategy/test/create-preload-strategy.spec.ts
@@ -1,7 +1,30 @@
 /// <reference types="jasmine" />
 
 import { createPreloadStrategy } from '../create-preload-strategy';
+import { PreloadNothingStrategy } from '../preload-nothing-strategy';
+import { FollowLinksPreloadStrategy } from '../follow-links-preload-strategy';
+import { PreloadEverythingStrategy } from '../preload-everything-strategy';
 
-describe('util.createPreloadStrategy', () => {
-    
+describe('util/createPreloadStrategy', () => {
+    it('should return a PreloadNothingStrategy if nothing is passed in', () => {
+        expect(createPreloadStrategy() instanceof PreloadNothingStrategy).toBe(true);
+    });
+    it(`should return a PreloadNothingStrategy if 'none' is passed in`, () => {
+        expect(createPreloadStrategy('none') instanceof PreloadNothingStrategy).toBe(true);
+    });
+    it(`should return a FollowLinksPreloadStrategy if 'follow-links' is passed in`, () => {
+        expect(createPreloadStrategy('follow-links') instanceof FollowLinksPreloadStrategy).toBe(true);
+    });
+    it(`should return a PreloadEverythingStrategy if 'all' is passed in`, () => {
+        expect(createPreloadStrategy('all') instanceof PreloadEverythingStrategy).toBe(true);
+    });
+    it(`should return the same instance if an instance of PreloadStrategy is passed in`, () => {
+        let inst = new PreloadNothingStrategy();
+        expect(createPreloadStrategy(inst)).toBe(inst);
+    });
+    it(`should throw an error if any other value is passed in`, () => {
+        expect(() => createPreloadStrategy(<any>'fish')).toThrowError(/invalid preload strategy/i);
+        expect(() => createPreloadStrategy(<any>'')).toThrowError(/invalid preload strategy/i);
+        expect(() => createPreloadStrategy(<any>{ key: 'value' })).toThrowError(/invalid preload strategy/i);
+    });
 });
